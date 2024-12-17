aaa02854916411225866149240194fb33a0db4a6
Tehreem Fazili <tehreemfazili@Tehreems-MacBook-Pro.local>
2024-12-17 13:24:38 +0530

    Created success story form, added ajax form

--- web/modules/custom/foodie_contact_form/src/Controller/FoodieController.php
+++ web/modules/custom/foodie_contact_form/src/Controller/FoodieController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Drupal\foodie_contact_form\Controller;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
 /**
 * Provides a controller class for the ultimate fitness challenge page.
 */
@@ -14,9 +15,29 @@ class FoodieController extends ControllerBase {
     */
 
     public function challengePage() {
-        return [
-            '#markup' => '<p>Welcome! Let\'s transform ourselves with daily workouts,
-            motivation, and community support in the ultimate fitness challenge.</p>',
-        ];
+
+      $modal_link = [
+        '#type' => 'link',
+        '#title' => $this->t('Submit My Success Story'),
+        '#url' => Url::fromRoute('foodie_contact_form.modal_form'),  // Define the route for the modal form.
+        '#attributes' => [
+          'class' => ['use-ajax'],  // Enable AJAX for the link.
+          'data-dialog-type' => 'modal',  // Specify that the dialog type is a modal.
+          'data-dialog-options' => json_encode(['width' => 700]),  // Optional: specify modal options like width.
+        ],
+        '#attached' => [
+          'library' => [
+              'core/drupal.dialog.ajax',  // Attach the necessary core library for AJAX modals.
+          ],
+        ],
+      ];
+
+      $welcome_markup = [ '#markup' => '<p>Welcome! Let\'s transform ourselves with daily workouts,
+      motivation, and community support in the ultimate fitness challenge.</p>' ];
+
+      return [
+        $modal_link,
+        $welcome_markup
+      ];
     }
 }
